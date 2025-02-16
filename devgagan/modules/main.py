904cd62cc75a9af2bf298407d5c360bfd7dcf834
Amit <heroku467@gmail.com>
2025-02-16 16:11:28 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -147,7 +147,7 @@ async def batch_link(_, message):
     keyboard = InlineKeyboardMarkup([[join_button]])
     pin_msg = await app.send_message(
         user_id,
-        "Batch process started ⚡\n__Processing: 0/{cl}__\n\n**__Powered by Team SPY__**",
+        "Batch process started ⚡\n__Processing: 0/{cl}__**",
         reply_markup=keyboard
     )
     try:
@@ -168,7 +168,7 @@ async def batch_link(_, message):
                         msg = await app.send_message(message.chat.id, f"Processing link {url}...")
                         await get_msg(None, user_id, msg.id, link, 0, message)
                         await pin_msg.edit_text(
-                        f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**__Powered by Team SPY__**",
+                        f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**",
                         reply_markup=keyboard
                         )
                         await asyncio.sleep(5)
@@ -179,7 +179,7 @@ async def batch_link(_, message):
             await set_interval(user_id, interval_minutes=20)
             await app.send_message(message.chat.id, "Batch completed successfully! 🎉")
             await pin_msg.edit_text(
-                        f"Batch process completed for {cl} messages enjoy 🌝\n\n**__Powered by Team SPY__**",
+                        f"Batch process completed for {cl} messages enjoy 🌝.**",
                         reply_markup=keyboard
             )
             return
@@ -217,7 +217,7 @@ async def batch_link(_, message):
                             )
                             await asyncio.sleep(2)
                             await pin_msg.edit_text(
-                            f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**__Powered by Team SPY__**",
+                            f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**",
                             reply_markup=keyboard
                             )
                             await asyncio.sleep(10)
@@ -231,7 +231,7 @@ async def batch_link(_, message):
         await app.send_message(message.chat.id, "Batch completed successfully! 🎉")
         await set_interval(user_id, interval_minutes=20)
         await pin_msg.edit_text(
-                        f"Batch completed for {cl} messages ⚡\n\n**__Powered by Team SPY__**",
+                        f"Batch completed for {cl} messages ⚡**",
                         reply_markup=keyboard
         )
     except FloodWait as fw:
